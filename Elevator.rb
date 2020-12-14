2cf786eca8bbce3c8914bd351d665d2eb57b7d0f
prince.titus.codjoe <prince.titus.codjoe@gmail.com>
2020-12-14 06:44:19 -0500

    next floor method

--- Elevator.rb
+++ Elevator.rb
@@ -10,6 +10,34 @@ class Elevator
   
   
   def next_floor(elev_set)
+    elev_array = elev_set.to_a
+    floor = nil
+    if @direction == Elevator::DIRECTION[:up]
+      if elev_array.last > @current_floor
+        floor = elev_array[@current_floor + 1]
+      else
+        floor = elev_array[@current_floor - 1]
+      end
+    else
+      if @current_floor > 0
+        floor = elev_array[@current_floor - 1] 
+      else
+        floor = elev_array[@current_floor + 1]
+      end
+    end
+    
+    if floor == nil
+      begin
+        puts "Waiting at Floor: " + @current_floor
+        Thread.stop
+      catch Exception => error
+        puts error.backtrace 
+      end
+    else
+      elev_set.delete(floor)
+    end
+
+    return (floor == nil) ? -1 : floor
   end
 
 
